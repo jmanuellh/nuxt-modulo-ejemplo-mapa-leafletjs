49e5eba156796ad159bb8df5cfae4334c005f498
manuel lopez <manuellh2008@gmail.com>
2020-12-02 18:13:35 -0600

    first commit

---
+++ components/moduloMapa/index.vue
@@ -0,0 +1,8 @@
+<template lang="pug">
+  div
+    div(id="map-wrap" style="height: 100vh")
+      no-ssr
+        l-map(:zoom=15 :center="[25.8682,-103.2429]" style="z-index:0")
+          l-tile-layer( url="http://{s}.tile.osm.org/{z}/{x}/{y}.png")
+          l-marker(:lat-lng="[25.8682,-103.2429]")
+</template>
